4f4636bcfb00a185232709d4261de9cf71927bcb
FlamingArr <53872662+FlamingArr@users.noreply.github.com>
2021-01-05 23:26:47 +0530

    update tests.lua

--- tests.lua
+++ tests.lua
@@ -2,6 +2,20 @@ local lust = require "lust"
 local Vec2 = require "TVec"
 local describe, it, expect = lust.describe, lust.it, lust.expect
 
+local v
 
-
-expect(6).to.equal(6)
+describe("TVec", function()
+	it("Vector creation", function()
+		v = Vec2()
+		expect(v.x).to.equal(0)
+		expect(v.y).to.equal(0)
+		
+		v = Vec2(8)
+		expect(v.x).to.equal(8)
+		expect(v.y).to.equal(8)
+		
+		v = Vec2(3, 4)
+		expect(v.x).to.equal(3)
+		expect(v.y).to.equal(4)
+	end)
+end)
