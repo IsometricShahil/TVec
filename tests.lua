00df23911c226c0bc6c5b1f3ccddf01cd6eb1071
FlamingArr <53872662+FlamingArr@users.noreply.github.com>
2021-02-11 22:38:12 +0530

    update tests.lua

--- tests.lua
+++ tests.lua
@@ -1,1 +1,14 @@
-print("Hello from", _VERSION)
+local lust = require "lust.lust"
+local describe = lust.describe
+local it = lust.it
+local expect = lust.expect
+
+
+describe("TVec tests", function()
+	it("Passing", function()
+		expect(56).to.be(56)
+	end)
+	it("Failing", function()
+		expect(50).to_not.be(50)
+	end)
+end)
