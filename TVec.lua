d304ecf4075c79bd1e353f4dfacdcb315642082a
FlamingArr <53872662+FlamingArr@users.noreply.github.com>
2021-02-12 10:14:20 +0530

    update TVec.lua

--- TVec.lua
+++ TVec.lua
@@ -334,7 +334,7 @@ local ops = {
 	{"mul", "*"},
 	{"div", "/"},
 	{"pow", "^"},
-	{"mod", "%"},
+	{"mod", "%%"}, --Needs to be escaped
 }
 
 local env = { --Environment to run the chunks in
@@ -350,7 +350,7 @@ for i = 1, #ops do
 	
 	--Do replacement
 	local genCode = sharedCode:gsub("NAME", v[1])
-	genCode = genCode:gsub("OP", "%"..v[2])
+	genCode = genCode:gsub("OP", v[2])
 	
 	--Load the chunk
 	local chunk, msg = load(genCode, "metaChunk", "t", env)
