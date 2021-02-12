5714eb35b1ea10b4a77594dc20596313efd562cd
FlamingArr <53872662+FlamingArr@users.noreply.github.com>
2021-02-12 10:35:41 +0530

    update tests.lua

--- tests.lua
+++ tests.lua
@@ -3,24 +3,30 @@ local Vec2 = require "TVec"
 local describe, it, expect = lust.describe, lust.it, lust.expect
 
 describe("TVec tests", function()
-	it("Constructor", function()
-		local v0 = Vec2()
-		local v5= Vec2(5)
-		expect(v0).to.be(Vec2(0, 0))
-		expect(v5).to.be(Vec2(5, 5))
+	describe("General", function()
+		it("Constructor", function()
+				local v0 = Vec2()
+				local v5= Vec2(5)
+				expect(v0).to.be(Vec2(0, 0))
+				expect(v5).to.be(Vec2(5, 5))
+		end)
+		
+		it("Type check", function()
+				local v = Vec2()
+				expect(Vec2.isVector(v)).to.be(true)
+				expect(Vec2.isVector({})).to.be(false)
+				expect(Vec2.isVector(nil)).to.be(false)
+		end)
+		
+		it("Pooling", function()
+				local v1 = Vec2()
+				v1:free()
+				expect(Vec2()).to.be(v1)
+		end)
 	end)
 	
-	it("Type check", function()
-		local v = Vec2()
-		expect(Vec2.isVector(v)).to.be(true)
-		expect(Vec2.isVector({})).to.be(false)
-		expect(Vec2.isVector(nil)).to.be(false)
-	end)
-	
-	it("Pooling", function()
-		local v1 = Vec2()
-		v1:free()
-		expect(Vec2()).to.be(v1)
+	describe("Mathematical", function()
+		
 	end)
 end)
 
